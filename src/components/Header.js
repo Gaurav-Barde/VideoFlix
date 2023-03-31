29c1c15d2a286de11004a450f93b5097a65eddd2
Gaurav-Barde <gabarde25@gmail.com>
2023-03-31 19:55:12 +0530

    Replaced images with React-Icons

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,12 +1,14 @@
 import React, { useEffect, useState } from "react";
-import MenuIcon from "../assets/img/menu-icon.png";
 import Logo from "../assets/img/logo.png";
-import UserIcon from "../assets/img/user-icon.png";
-import SearchIcon from "../assets/img/search-icon.png";
 import { useDispatch } from "react-redux";
 import { toggleMenu } from "../utils/redux/menuSlice";
 import { YOUTUBE_SEARCH_SUGGESTION_API } from "../utils/constants";
-import { RxMagnifyingGlass, RxCross1 } from "react-icons/rx";
+import {
+  RxMagnifyingGlass,
+  RxCross1,
+  RxHamburgerMenu,
+  RxAvatar,
+} from "react-icons/rx";
 
 const Header = () => {
   const [searchTerm, setSearchTerm] = useState("");
@@ -35,7 +37,7 @@ const Header = () => {
     <header className="grid grid-cols-12 items-center">
       <div className="col-span-3 flex items-center">
         <button onClick={toggleMenuHandler}>
-          <img alt="menu" src={MenuIcon} className="h-6 mr-4 cursor-pointer" />
+          <RxHamburgerMenu className="text-3xl" />
         </button>
         <img alt="logo" src={Logo} className="h-5 mx-2 cursor-pointer" />
       </div>
@@ -58,7 +60,7 @@ const Header = () => {
           )}
         </div>
         <button className="bg-slate-200 rounded-r-md p-2">
-          <img alt="search" src={SearchIcon} className="h-6" />
+          <RxMagnifyingGlass className="text-2xl" />
         </button>
         {/* Suggestions Modal */}
         {searchTerm && (
@@ -73,8 +75,8 @@ const Header = () => {
         )}
       </div>
       <div className="col-span-3 flex justify-end">
-        <button className="flex items-center border border-gray-150 p-1 rounded-md">
-          <img alt="user" src={UserIcon} className="h-6" />
+        <button className="flex items-center border border-gray-150 p-2 rounded-md">
+          <RxAvatar className="text-2xl" />
           <span className="text-sm font-bold ml-1 text-blue-600">Sign in</span>
         </button>
       </div>
